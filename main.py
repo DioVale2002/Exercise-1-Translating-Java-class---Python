562d67129385016b096ccd980f69534a80b1c9b5
DioVale2002 <64457128+DioVale2002@users.noreply.github.com>
2025-10-28 21:58:31 +0800

    add the bark function to make the object woof

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ class Dog:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-   
+
+    def bark():
+        print("Woof! Woof!")
+    def celebateBirthday():
+        
+    def getInfo():
+
+    
+
+
