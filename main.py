e3f24835b397fee0ebbe1472de969fe229a8cf93
DioVale2002 <64457128+DioVale2002@users.noreply.github.com>
2025-10-28 22:03:17 +0800

    add functions to celebrate dog's birthday and to print their basic information

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ class Dog:
 
     def bark():
         print("Woof! Woof!")
+
     def celebateBirthday():
+        self.age += age
+        print("Happy birthday!" + " " + self.name +". You are now " + self.age + " old!")
         
     def getInfo():
+        print(self.name)
+        print(str(self.age))
+    
+
 
     
 
