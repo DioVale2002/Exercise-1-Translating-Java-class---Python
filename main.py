24d3b9e67b7610c08367ed094171f9a0e69ff116
DioVale2002 <64457128+DioVale2002@users.noreply.github.com>
2025-10-28 22:07:17 +0800

    fix addition bug

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ class Dog:
         self.name = name
         self.age = age
 
-    def bark():
+    def bark(self):
         print("Woof! Woof!")
 
-    def celebateBirthday():
-        self.age += age
-        print("Happy birthday!" + " " + self.name +". You are now " + self.age + " old!")
-        
-    def getInfo():
+    def celebateBirthday(self):
+        self.age += 1
+        print("Happy birthday!" + " " + self.name +". You are now " + str(self.age) + " old!")
+
+    def getInfo(self):
         print(self.name)
         print(str(self.age))
     
+doog1 = Dog("Doog", 3)
 
+doog1.bark()
+doog1.celebateBirthday()
+doog1.getInfo()
 
     
 
